ee1a48734253180538146cdf39bb39887d07a25b
Shilkey <kierans.borg@gmail.com>
2025-11-22 20:56:20 +0100

    added icon

--- SekiroTool/App.xaml.cs
+++ SekiroTool/App.xaml.cs
@@ -7,7 +7,21 @@ namespace SekiroTool;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App
 {
-    
+    private static Mutex _mutex;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        const string appName = "SekiroTool";
+
+        _mutex = new Mutex(true, appName, out var createdNew);
+
+        if (!createdNew)
+        {
+            Current.Shutdown();
+        }
+
+        base.OnStartup(e);
+    }       
 }
