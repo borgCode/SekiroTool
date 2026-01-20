8e364da52b7391bd43665e3f2e8802d26da918cc
Shilkey <kierans.borg@gmail.com>
2026-01-20 21:32:46 +0100

    fixed damage multiplier surely this time

--- SekiroTool/Services/PlayerService.cs
+++ SekiroTool/Services/PlayerService.cs
@@ -444,9 +444,9 @@ public class PlayerService(IMemoryService memoryService, HookManager hookManager
             var origin = Hooks.DamageMultiplier;
             AsmHelper.WriteRelativeOffsets(bytes, [
             (code.ToInt64() + 0x8, WorldChrMan.Base.ToInt64(), 7, 0x8 + 3),
-            (code.ToInt64() + 0x25, damageMultiplier.ToInt64(), 9, 0x25 + 5),
-            (code.ToInt64() + 0x46, damageMultiplier.ToInt64(), 9, 0x46 + 5),
-            (code.ToInt64() + 0x5D, origin + 7, 5, 0x5D + 1)
+            (code.ToInt64() + 0x2C, damageMultiplier.ToInt64(), 9, 0x2C + 5),
+            (code.ToInt64() + 0x4D, damageMultiplier.ToInt64(), 9, 0x4D + 5),
+            (code.ToInt64() + 0x64, origin + 7, 5, 0x64 + 1)
             ]);
             
             memoryService.WriteBytes(code, bytes);
